2306b58caa274811953c2f18f8990a2185ee3e08
Harish Anand <harishanand95@gmail.com>
2018-02-09 01:07:07 +0530

    Fix: avoid swap creation for test setup
    * by default, swap creation avoided if enough RAM is there
    
    Signed-off-by: Harish Anand <harishanand95@gmail.com>

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 sudo apt-get update && sudo apt-get install -y software-properties-common && sudo apt-add-repository ppa:ansible/ansible && sudo apt-get update && sudo apt-get install -y ansible
 sudo apt-get install -y apt-transport-https  ca-certificates curl software-properties-common
-git clone https://github.com/rbccps-iisc/smartcity-middleware-docker.git
-apt install python-pip python -y
+sudo apt install python-pip python -y
 python -m pip install pathlib2
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce -y
-mkdir -p /data/kong
-mkdir -p /data/kong-config
-mkdir -p /data/catalogue
-mkdir -p /data/rabbitmq
-mkdir -p /data/ldap
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo mkdir -p /data/kong
+sudo mkdir -p /data/kong-config
+sudo mkdir -p /data/catalogue
+sudo mkdir -p /data/rabbitmq
+sudo mkdir -p /data/ldap
+sudo mkdir -p /data/tomcat
+sudo mkdir -p /data/logs/kong
+sudo mkdir -p /data/logs/rabbitmq
+sudo mkdir -p /data/logs/tomcat
+sudo usermod -aG docker $USER
 ssh-keygen
-chmod 777 /data/*
-dd if=/dev/zero of=/swapfile bs=1024 count=2048k
-mkswap /swapfile
-swapon /swapfile
-echo 10 | sudo tee /proc/sys/vm/swappiness
-echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
-chown root:root /swapfile
-chmod 0600 /swapfile
+sudo chmod -R 777 /data/*
+sudo csysctl -w vm.max_map_count=662144
