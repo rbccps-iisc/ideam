3a4964fc22f5fa3c963646e9b9a9e53c9d02a95e
Poorna Chandra Tejasvi KG <pct960@gmail.com>
2018-04-12 22:47:02 +0530

    Integrated IDPS setup with IDEAM installation (#64)
    Bugfix: Reordered tasks in ansible playbook
    
    Signed off by: Poorna Chandra Tejasvi<pct960@gmail.com>

--- modules/start.py
+++ modules/start.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess,os
 from create_hosts import create_hosts
 
 
@@ -8,7 +8,17 @@ def start_all():
     subprocess.call("docker start konga", shell=True)
     subprocess.call('ansible-playbook -i hosts start.yaml', shell=True)
 
+    if os.getlogin()=='travis':
+        subprocess.call('ansible-playbook -i \'localhost\' -s install_idps_travis.yml', shell=True)
+    else:
+        subprocess.call('ansible-playbook -i \'localhost\' -s install_idps.yml --ask-sudo-pass',shell=True)
+
 
 def ansible_start(limit=""):
     create_hosts()
     subprocess.call('ansible-playbook -i hosts start.yaml --limit "' + limit + '"', shell=True)
+    
+    if os.getlogin()=='travis':
+        subprocess.call('ansible-playbook -i \'localhost\' -s install_idps_travis.yml', shell=True)
+    else:
+        subprocess.call('ansible-playbook -i \'localhost\' -s install_idps.yml --ask-sudo-pass',shell=True)
