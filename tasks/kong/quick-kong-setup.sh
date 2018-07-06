18e2aa973564ef4c1b9a007630eba6ad6051ab96
Poorna Chandra Tejasvi KG <pct960@gmail.com>
2018-07-06 19:54:14 +0530

    Bugfix: Removed lock file from kong container (#126)
    * Bugfix: Removed lock file from kong container
    
    * Redirected pg_isready to /dev/null

--- tasks/kong/quick-kong-setup.sh
+++ tasks/kong/quick-kong-setup.sh
@@ -1,18 +1,17 @@
 #!/bin/ash
 echo "TrustedUserCAKeys /etc/ssh/ca-user-certificate-key.pub" >> /etc/ssh/sshd_config
 rm -r /tmp/tmux-*
-
 su postgres -c "/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data > /var/lib/postgresql/logfile 2>&1 &"
 
-until psql --host=localhost --username=postgres postgres -w &>/dev/null
-do
-sleep 0.1
-done
-
-#until 'pg_isready' 2>/dev/null; do
-#  >&2 echo "Postgres is unavailable - sleeping for 0.1 seconds"
-#  sleep 0.1
+#until psql --host=localhost --username=postgres postgres -w 
+#do
+#sleep 0.1
 #done
+
+until su postgres -c 'pg_isready' >/dev/null 2>&1
+do
+  sleep 0.1
+done
 
 kong start -c /etc/kong/kong.conf
 
@@ -27,3 +26,4 @@ sed -i 's/rmq_user/admin.ideam/g' /home/ideam/share.py
 sed -i 's/rmq_pwd/'$rmqpwd'/g' /home/ideam/share.py
 sed -i 's/ldap_pwd/'$ldapdpwd'/g' /home/ideam/share.py
 tmux new-session -d -s share 'python3.6 /home/ideam/share.py'
+
