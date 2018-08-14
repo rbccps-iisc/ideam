0b75a71e61b4812d0a948d7f0d429a055469684b
Vasanth Rajaraman <swaminathanvasanth.r@gmail.com>
2018-08-14 13:10:10 +0530

    Updated webserver installation to include error and stdout log files (#153)

--- tasks/webserver/install.sh
+++ tasks/webserver/install.sh
@@ -5,12 +5,44 @@ NC='\033[0m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 
+
+echo -e "${YELLOW}[  INFO  ]${NC} Creating Error Log File"
+
+touch /var/log/cdxapis_error.log
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[   OK   ]${NC} Created Error Log File"
+else
+    echo -e "${RED}[ ERROR ]${NC} Failed to Create Error Log File"
+fi
+
+echo -e "${YELLOW}[  INFO  ]${NC} Creating Out Log File"
+
+touch /var/log/cdxapis_out.log
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[   OK   ]${NC} Created Out Log File"
+else
+    echo -e "${RED}[ ERROR ]${NC} Failed to Create Out Log File"
+fi
+
+echo -e "${YELLOW}[  INFO  ]${NC} Changing Permission of Log File"
+
+chmod +x /var/log/cdxapis_*
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[   OK   ]${NC} Success Changing permission"
+else
+    echo -e "${RED}[ ERROR ]${NC} Failed to change permission"
+fi
+
 echo -e "${YELLOW}[  INFO  ]${NC} Starting Webserver"
 
-nohup java -jar /usr/local/webserver/cdxapis.jar 2>/dev/null >/dev/null &
+nohup java -jar /usr/local/webserver/cdxapis.jar 2>/var/log/cdxapis_error.log >/var/log/cdxapis_out.log &
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[   OK   ]${NC} Started Webserver"
+    echo -e "${YELLOW}[   INFO   ]${NC} Webserver Logs are at /var/log/"
 else
     echo -e "${RED}[ ERROR ]${NC} Failed to start Webserver"
 fi
