cb85041b7746d5c1ee00af77fe324e44f842a528
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-09-22 11:08:27 +0530

    Removed Ubuntu related files (#236)

--- ubuntu/tasks/certificate_authority/ca.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker cp config/certificate_authority/keys/id_rsa.pub certificate_authority:/root/
-ssh -o "StrictHostKeyChecking=no" root@localhost -p $1 < tasks/certificate_authority/sign.sh
-docker cp certificate_authority:/root/id_rsa-cert.pub config/certificate_authority/keys
-docker cp certificate_authority:/root/.ssh/ca-user-certificate-key.pub config/certificate_authority/keys/ 
-docker cp certificate_authority:/root/id_rsa-cert.pub ~/.ssh/
